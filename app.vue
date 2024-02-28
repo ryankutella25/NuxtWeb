efb76bdf774ffbd5f3087997739403af37d74b93
ryankutella25 <ryankutella25@yahoo.com>
2024-02-28 04:02:18 -0600

    starting mobile work

--- app.vue
+++ app.vue
@@ -1,10 +1,20 @@
-<script>
+<script setup>
     import '~/assets/css/main.css';
+    const isOpen = ref(false)
 </script>
 
 <template>
   <div class="fullWidth">
-    <AppHeader /> <!-- Header (in components) -->
+    <UButton label="Tabs" class="burger" @click="isOpen = true" />
+
+    
+    <AppHeader class="mainHeader"/> <!-- Header (in components) --> 
     <NuxtPage /> <!-- Page Content (in pages) -->
+
+    <USlideover side="left" v-model="isOpen">
+      <div class="p-4 flex-1">  
+        <!-- <Placeholder class="h-full" />  -->
+      </div>
+    </USlideover>
   </div>
 </template>
