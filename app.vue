41aadfc8a2443a5ffcdbb181d6115d07169caa29
ryankutella25 <ryankutella25@yahoo.com>
2024-02-28 14:41:34 -0600

    fixing mobile

--- app.vue
+++ app.vue
@@ -1,20 +1,15 @@
 <script setup>
     import '~/assets/css/main.css';
-    const isOpen = ref(false)
+
 </script>
 
 <template>
   <div class="fullWidth">
-    <UButton label="Tabs" class="burger" @click="isOpen = true" />
 
-    
-    <AppHeader class="mainHeader"/> <!-- Header (in components) --> 
+    <MobileHeader class="mobileHeader"/>
+    <MainHeader class="mainHeader"/> <!-- Header (in components) --> 
     <NuxtPage /> <!-- Page Content (in pages) -->
 
-    <USlideover side="left" v-model="isOpen">
-      <div class="p-4 flex-1">  
-        <!-- <Placeholder class="h-full" />  -->
-      </div>
-    </USlideover>
+    
   </div>
 </template>
---
+++ components/MobileHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+    import '~/assets/css/mobileheader.css';
+
+    const isOpen = ref(false)
+
+</script>
+
+<template>
+  <div >
+    <div class="mobileHeaderContainer">
+        <UButton label="Tabs" class="burger" @click="isOpen = true" />
+
+        <div class="rightHeader">Ryan Kutella</div>
+    </div>
+    
+
+
+    <USlideover class="slideInPage" side="left" v-model="isOpen">
+        <div class="mobileTabs">
+            <NuxtLink class="mobileTabText" to="/">Home</NuxtLink>
+            <NuxtLink class="mobileTabText" to="/projects">Projects</NuxtLink>
+            <NuxtLink class="mobileTabText" to="/aboutme">About Me</NuxtLink>
+        </div>
+
+        <div class="mobileSocials">
+          <UButton class="socialButton" :padded="false" variant="ghost" color="white" to="https://www.linkedin.com/in/ryankutella/" target="_blank"><Icon name="uil:linkedin" color="#ddd" size="24px"/>LinkedIn</UButton>
+          <UButton class="socialButton" :padded="false" variant="ghost" color="white" to="mailto:ryankutella25@yahoo.com" target="_blank"><Icon name="ic:baseline-mail-outline" color="#ddd" size="24px"/>Email</UButton>
+          <UButton class="socialButton" :padded="false" variant="ghost" color="white" to="https://github.com/ryankutella25" target="_blank"><Icon name="uil:github" color="#ddd" size="24px"/>GitHub</UButton>
+          <UButton class="socialButton" color="cyan" variant="outline" to="/RyanKutellaResume.pdf" target="_blank">Resume</UButton>
+        </div>
+    </USlideover>
+  </div>
+</template>
