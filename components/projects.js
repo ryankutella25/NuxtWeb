69d45d8594e31102fd4a9feebb27cf66638be30a
ryankutella25 <ryankutella25@yahoo.com>
2024-02-28 22:05:16 -0600

    Dropdowns

--- components/projects.js
+++ components/projects.js
@@ -1,27 +1,34 @@
 
 const items = [{
     src: 'https://picsum.photos/600/600?random=1',
+    tech: ['Java','Python'],
+    type: 'Personal',
     active: true,
     itemNum: 0,
   },
   {
     src: 'https://picsum.photos/600/600?random=2',
+    tech: ['Java','Python'],
+    type: 'Personal',
     active: false,
     itemNum: 1,
   },
   {
     src: 'https://picsum.photos/600/600?random=3',
+    tech: ['Java'],
     active: false,
     itemNum: 2,
   },
   {
     src: 'https://picsum.photos/600/600?random=4',
+    tech: ['Python'],
     active: false,
     itemNum: 3,
   },
   {
     src: 'https://picsum.photos/600/600?random=3',
     active: false,
+    type: 'Personal',
     itemNum: 4,
   },
   {
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-
     import '~/assets/css/projects.css';
-
-    import projects from "~/components/projects.js";
+import projects from "~/components/projects.js";
 
     //items are project items in carousel
     const items = [{}]
@@ -11,33 +9,70 @@
       items.push({});
     }
 
+    const languagesSelected = ref([]);
+    const languages = ['Java', 'Python'];
 
-onMounted(() => {
-  fixBackgroundHeight(),
-  //fixes carousel after resizing window
-  window.addEventListener("resize", () => {
-    fixBackgroundHeight();
-  }),
-  //fixes carousel when going fullscreen
-  window.addEventListener("fullscreenchange", function() {
-    fixBackgroundHeight();
-  });
-})
+    const projectTypesSelected = ref([]);
+    const projectType = ['Personal', 'School'];
 
+  onMounted(() => {
+    fixBackgroundHeight(),
+    //fixes carousel after resizing window
+    window.addEventListener("resize", () => {
+      fixBackgroundHeight();
+    }),
+    //fixes carousel when going fullscreen
+    window.addEventListener("fullscreenchange", function() {
+      fixBackgroundHeight();
+    });
+  })
 
-//fixes carousel and lets active item be on screen
-function fixBackgroundHeight(){
-  var projectsBackground = document.getElementById("projectsBackground");
-  if(projectsBackground==null){
-    return;
+  //fixes carousel and lets active item be on screen
+  function fixBackgroundHeight(){
+    var projectsBackground = document.getElementById("projectsBackground");
+    if(projectsBackground==null){
+      return;
+    }
+    var body = document.body;
+    if(body==null){
+      return;
+    }
+
+    projectsBackground.style.height = body.offsetHeight+'px';
   }
-  var body = document.body;
-  if(body==null){
-    return;
-  }
 
-  projectsBackground.style.height = body.offsetHeight+'px';
-}
+  const sortedProjects = computed(() => {
+    return projects.filter((item)=> {
+
+      //make sure it fits sorting tech
+      let techTrue = false;
+      if(languagesSelected.value.length!=0){
+        if(item.tech==null) return false;
+        for(let i = 0; i < item.tech.length && !techTrue; i++){
+          for(let j = 0; j < languagesSelected.value.length; j++){
+            console.log(item.tech[0]+" and "+languagesSelected.value[j])
+            if(item.tech[0]==languagesSelected.value[j]){
+              techTrue = true;
+              break;
+            };
+          }
+        }
+      }else techTrue = true;
+
+      let typeTrue = false;
+      if(projectTypesSelected.value.length!=0){
+        if(item.type==null) return false;
+        for(let j = 0; j < projectTypesSelected.value.length; j++){
+          if(item.type==projectTypesSelected.value[j]){
+            typeTrue = true;
+            break;
+          }
+        }
+      }else typeTrue=true;
+
+      return techTrue&&typeTrue;
+    })
+  })
 
 </script>
 
@@ -45,9 +80,28 @@ function fixBackgroundHeight(){
   <div class="projectPageContainer">
     <div class="background" id="projectsBackground"><span v-for="item in items" style="font-weight: bolder;"> &lt;Ryan_Kutella&gt; </span></div>
     <!-- This page correctly has only one single root element -->
-    <div class="projectContainer" id="projectContainer">
-      <div class="card" v-for="item in projects">
+    <div class="selectMenuContainer">
+      <USelectMenu variant="none" class="selectMenus" style="z-index: 2;" v-model="languagesSelected" :options="languages" multiple placeholder="Technology" >
+        <template #label>
+          <span v-if="languagesSelected.length" class="truncate">{{ languagesSelected.join(', ') }}</span>
+          <span v-else>Technology</span>
+        </template>
+      </USelectMenu>
+      <USelectMenu variant="none" class="selectMenus" style="z-index: 2;" v-model="projectTypesSelected" :options="projectType" multiple placeholder="Personal/School" >
+        <template #label>
+          <span v-if="projectTypesSelected.length" class="truncate">{{ projectTypesSelected.join(', ') }}</span>
+          <span v-else>Personal/School</span>
+        </template>
+      </USelectMenu>
+    </div>
 
+
+    <div class="projectContainer" id="projectContainer">
+      <div class="card" v-for="item in sortedProjects" >
+        {{ item.itemNum }}
+        <div v-for="tech in item.tech">
+          {{ tech }}
+        </div>
       </div>
     </div>
   </div>
