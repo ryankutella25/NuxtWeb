f0630fea0067ab4ff056f025bdba7af23ca8dcc5
ryankutella25 <ryankutella25@yahoo.com>
2024-03-05 16:31:29 -0600

    title update

--- app.vue
+++ app.vue
@@ -68,6 +68,7 @@
       }
     }
 
+    useHead({title: 'Ryan Kutella\'s Portfolio'})
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-icon','@nuxt/ui'],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
 })
