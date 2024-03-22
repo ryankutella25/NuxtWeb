2da2c2174a868ad2fe855bc5a4b2325623809ad7
ryankutella25 <ryankutella25@yahoo.com>
2024-03-22 10:59:17 -0500

    stopped console logging

--- pages/projects.vue
+++ pages/projects.vue
@@ -23,7 +23,7 @@ const sortedProjects = computed(() => {
       if (item.tech == null) return false;
       for (let i = 0; i < item.tech.length && !techTrue; i++) {
         for (let j = 0; j < languagesSelected.value.length; j++) {
-          console.log(item.tech[i] + " and " + languagesSelected.value[j]);
+          // console.log(item.tech[i] + " and " + languagesSelected.value[j]);
           if (item.tech[i] == languagesSelected.value[j]) {
             techTrue = true;
             break;
