f690e580492950b11c6f5d510a09d2fc2902d774
ryankutella25 <ryankutella25@yahoo.com>
2024-05-18 15:02:31 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,11 @@ export default defineNuxtConfig({
       title: 'Ryan Kutella\'s Portfolio',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      // link:  [{ rel: 'icon', type: 'image/x-icon', href: '/RKLogo.png' }]
+      link:  [{ rel: 'apple-touch-icon', sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: 'icon', type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: 'icon', type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: 'manifest', href: "/site.webmanifest" },
+      { rel: 'mask-icon', href: "/safari-pinned-tab.svg", color: "#5bbad5" }],
     }
   }
 })
