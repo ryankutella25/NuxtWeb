47a98344bc9a032d840f1cee06ae1cf5f9cdb1ce
ryankutella25 <ryankutella25@yahoo.com>
2024-06-07 22:46:52 -0500

    small

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,12 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en',
       },
-      link:  [{ rel: 'icon', type: 'image/x-icon', sizes: "any", href: '/favicon.ico' },
+      link:  [
+      // { rel: 'icon', type: 'image/x-icon', sizes: "any", href: '/favicon.ico' },
       { rel: 'apple-touch-icon', sizes: "180x180", href: "/apple-touch-icon.png" },
       { rel: 'icon', type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
       { rel: 'icon', type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-      { rel: 'manifest', href: "/site.webmanifest" },
+      { rel: 'manifest', crossorigin: "use-credentials", href: "/site.webmanifest" },
       { rel: 'mask-icon', href: "/safari-pinned-tab.svg", color: "#5bbad5" }],
     }
   }
