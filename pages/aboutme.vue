db550167948be0e22c18d65e99e53b06e6b4c597
ryankutella25 <ryankutella25@yahoo.com>
2024-07-16 19:44:50 -0500

    fixed missing end tag error

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -1,48 +1,81 @@
-<script? setup lang="ts">
-    import '~/assets/css/aboutme.css';
-    useHead({
-      title: 'Ryan Kutella\'s Projects',
-      meta: [
-        { name: 'description', content: 'Full portfolio of my projects throughout the years.' }
-      ]
-    });
+<script setup lang="ts">
+  import "~/assets/css/aboutme.css";
+  useHead({
+    title: "Ryan Kutella's Projects",
+    meta: [
+      {
+        name: "description",
+        content: "Full portfolio of my projects throughout the years.",
+      },
+    ],
+  });
 </script>
 
 <template>
-  <div style="background-color: #202020;">
-    <div class="container">
-            <div class="about1">
-                <div class="about1_1">I am <span style="font-weight: bold; color: cyan;">Ryan Kutella</span> from Elk Grove Village, IL. 
-                    I am currently working towards my Computer Science Degree at The University of Alabama. 
-                    I am passionate about software development and like turning creative ideas into a reality. 
-                    I am an incoming senior and have been coding programs for over 7 years, starting freshman year of 
-                    high school. I have been adding different skills and programming languages into my arsenal as I go. 
-                    I've played a large role in the web development of a multi-million dollar company and have multiple other projects displaying my skills.
-                </div>
-                <div class="about1_2">
-                  <img class="aboutImage" alt="Bitmoji of Me" src="/sticker.webp"/>
-                </div>
-                
-            </div>
-            <div class="about2">
-               <div class="about2_1">
-                  <img class="skillsImage" alt="Lightbulb" src="/skills.webp"/>
-                </div>
-                <div class="about2_2">
-                  <p><span style="font-weight: bold; color: cyan;" >Technical Skills</span></p>
-                  <p><span>Languages: </span>JavaScript, C++, C#, Python, Java, HTML/CSS, SQL, Assembly, VHDL</p>
-                  <p><span>Frameworks: </span>React Native & React, Node.js, Nuxt.js, Angular, Vue</p>
-                  <p><span>Developer Tools: </span>Git/GitHub/Bitbucket, MongoDB, Oracle SQL & Amazon Firebase Databases, VSCode, JetBrains, JUnit</p>
-                  <p><span>UI/UX & Engineering Tools: </span>Figma, Blender, CAD, Autodesk, Unity, Microsoft Excel, Adobe Creative Cloud</p>
-                </div>
-            </div>
-            <div class="about3">
-                <div style="font-weight: bold; color: cyan; margin-bottom: 10px;">Experience: Software Developer/IT Intern at Abbott Rubber Co.</div>
-                <div>- Play a large role in the design and development of a new ecommerce website using HTML, CSS, and JS</div>
-                <div>- Create a streamlined user experience for both B2B and B2C interactions to increase conversion rates</div>
-                <div>- Work with software like Azure and Epicor Prophet 21 to help strengthen and expand internal systems.</div>
-                <div>- Assist with coworkers’ technical needs and upgrade existing systems to maintain and increase productivity.</div>
-            </div>
-    </div>          
+  <div class="container">
+    <div class="about1">
+      <div class="about1_1">
+        I am
+        <span style="font-weight: bold; color: cyan">Ryan Kutella</span> from
+        Elk Grove Village, IL. I am currently working towards my Computer
+        Science Degree at The University of Alabama. I am passionate about
+        software development and like turning creative ideas into a reality. I
+        am an incoming senior and have been coding programs for over 7 years,
+        starting freshman year of high school. I have been adding different
+        skills and programming languages into my arsenal as I go. I've played a
+        large role in the web development of a multi-million dollar company and
+        have multiple other projects displaying my skills.
+      </div>
+      <div class="about1_2">
+        <img class="aboutImage" alt="Bitmoji of Me" src="/sticker.webp" />
+      </div>
+    </div>
+    <div class="about2">
+      <div class="about2_1">
+        <img class="skillsImage" alt="Lightbulb" src="/skills.webp" />
+      </div>
+      <div class="about2_2">
+        <p>
+          <span style="font-weight: bold; color: cyan">Technical Skills</span>
+        </p>
+        <p>
+          <span>Languages: </span>JavaScript, C++, C#, Python, Java, HTML/CSS,
+          SQL, Assembly, VHDL
+        </p>
+        <p>
+          <span>Frameworks: </span>React Native & React, Node.js, Nuxt.js,
+          Angular, Vue
+        </p>
+        <p>
+          <span>Developer Tools: </span>Git/GitHub/Bitbucket, MongoDB, Oracle
+          SQL & Amazon Firebase Databases, VSCode, JetBrains, JUnit
+        </p>
+        <p>
+          <span>UI/UX & Engineering Tools: </span>Figma, Blender, CAD, Autodesk,
+          Unity, Microsoft Excel, Adobe Creative Cloud
+        </p>
+      </div>
+    </div>
+    <div class="about3">
+      <div style="font-weight: bold; color: cyan; margin-bottom: 10px">
+        Experience: Software Developer/IT Intern at Abbott Rubber Co.
+      </div>
+      <div>
+        - Play a large role in the design and development of a new ecommerce
+        website using HTML, CSS, and JS
+      </div>
+      <div>
+        - Create a streamlined user experience for both B2B and B2C interactions
+        to increase conversion rates
+      </div>
+      <div>
+        - Work with software like Azure and Epicor Prophet 21 to help strengthen
+        and expand internal systems.
+      </div>
+      <div>
+        - Assist with coworkers’ technical needs and upgrade existing systems to
+        maintain and increase productivity.
+      </div>
+    </div>
   </div>
 </template>
